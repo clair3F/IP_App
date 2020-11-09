e29ff7f79e85274e8b386732e7400f5e2f075073
cxf717 <cxf717@cs.bham.ac.uk>
2020-11-09 20:08:42 +0000

    Add checking of old and new directory but no compare yet

--- pexip_client.py
+++ pexip_client.py
@@ -15,6 +15,27 @@ def find_path(directory): #currently assumes the file is in the same directory a
     return path
 
 
+#https://www.thepythoncode.com/article/send-receive-files-using-sockets-python info on sending/receiving files as need to send in sections
+
+
+def dir_info(path):
+    old = {}
+    for file in os.listdir(path):
+        status = os.stat(file)
+        mod = status.st_mtime
+        old[file] = mod
+    
+    return old
+
+
+def update():
+
+
+def add():
+
+
+def remove():
+
 
 def main():
     print("Starting Client")
@@ -22,15 +43,22 @@ def main():
     directory = sys.argv[0] #directory from arguments on command line
     path = find_path(directory)
 
-    #use os.listdir and os.stat() and then status.st_mtime 
-    old = 
+    #getting the old info, could do in a function
+    old = dir_info(path)
     while true:
-        time.sleep(5) #waits and then gets new info from the 
-    #function to monitor the directory 
-    #dict store all files before and also all their mod times
+        time.sleep(5)
+        new = dir_info(path)
+        #now check if info has changed and if so call correct 
+
+
+
+
+
+    #dict store all filenames before and also all their mod times
     #then wait
-    #then compare all files check for new and check for old at same time if find it then check updated 
+    #then compare all filenames check for new and check for old at same time if find it then check updated 
     #when find one then call event to send to server command and file name 
+    #file = open() then data = read() then send this along with filename 
 
 
 
