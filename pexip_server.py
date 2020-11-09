9669e3da6ab552a92fe97448d4aa1ba544e404e5
cxf717 <cxf717@cs.bham.ac.uk>
2020-11-09 19:08:52 +0000

    Add make directory and server commands
    Untested suggestions for remove, add, update files on server side directory.

--- pexip_server.py
+++ pexip_server.py
@@ -5,13 +5,54 @@
 
 import sys #needed for arguments 
 import socket
+import os
+
+
+def make_directory():
+    cwdir = os.chdir() 
+    path = os.path.join(cwdir, dir)
+    os.mkdir(path) #note this takes a path and also throws OSError if directory exists. Handle this.
+    return path
+
+
+def update(data, filename):
+    os.remove(filename)
+    file = open(filename, 'wb')
+    file.write(data)
+    #also need to check how much data can be sent at once may need to loop through receiving file/writing to file
+
+    #add check that created file?
+
+
+
+def remove(filename):
+    os.remove(filename)
+
+
+def add(data, filename):
+    file = open(filename, 'wb')
+    file.write(data)
+    #also need to check how much data can be sent at once may need to loop through receiving file/writing to file
+
+    #add check that created file?
 
 
 def main():
     print("Starting Server")
 
-    #First step: constantly listening server 
 
+    #Make directory currently in same path as the server.
+    dir = sys.argv[0]
+    path = make_directory(dir)
+    os.chdir(path) #move into this directory 
+
+
+    #listening server
+    #parse data look for command. if update, add, remove then call function
+    #else break and close
+
+
+    #First step: constantly listening server 
     HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
     PORT = 8080      # Port to listen on (non-privileged ports are > 1023)
 
@@ -25,6 +66,9 @@ def main():
     while True:
         data = conn.recv(4096)
         print("Received data")
+
+        #checks on the data in here
+
         if not data:
             break #break if what is received is not correct format as expecting
 
