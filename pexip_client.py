aae78de9d299659a5d0d6268981103743a2cf610
cxf717 <cxf717@cs.bham.ac.uk>
2020-11-09 18:32:50 +0000

    Remove additional comments

--- pexip_client.py
+++ pexip_client.py
@@ -50,20 +50,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-################### Plan:
-#DECISION: Always connected or open and close connections bsed on time elapsed/always. Depends on how often the files system is updated.
-
-#Set monitoring directory based on the argument passed in 
-
-#Monitor this directory constantly and then trigger events
-
-#event: update <files> -> send update relevant files
-#event: delete <files> -> send delete relevant files
-#def update():
-#def delete():
-
-#in later versions will update these events to be more optimal
