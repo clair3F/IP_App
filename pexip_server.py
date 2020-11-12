4ae1a52b083e67838e333d809c17d67cf085e589
cxf717 <cxf717@cs.bham.ac.uk>
2020-11-12 16:53:59 +0000

    Add receiving file and command code untested

--- pexip_server.py
+++ pexip_server.py
@@ -7,70 +7,115 @@ import sys #needed for arguments
 import socket
 import os
 
+BUFFER = 4096
 
-def make_directory():
+def make_directory(dir):
     cwdir = os.chdir() 
     path = os.path.join(cwdir, dir)
     os.mkdir(path) #note this takes a path and also throws OSError if directory exists. Handle this.
     return path
 
 
-def update(file, filename):
-    os.remove(filename)
-    add(file, filename)
+
+def update(filename):
+    print("Performing update command")
+    print("Removing old file and adding new")
+    remove(filename)
+    add(filename)
+
 
 
 def remove(filename):
-    os.remove(filename)
+    print("Performing remove command")
+    os.remove(filename)#note may need to be a path but are in correct directory...
 
 
-def add(file, filename):
-    #change file already written to correct filename
+
+def add(filename):
+    print("Performing add command")
+    cwdir = os.chdir() 
+    oldpath = os.path.join(cwdir, "temporary")
+    newpath = os.path.join(cwdir, filename)
+    os.rename(oldpath, newpath)
 
 
-def write_file(file, data):
-    file.write(data)
-    #use this to create a file from the incoming file info in a loop. called if update or add.
+
+def receive_file(conn, filesize):
+    print("Receiving file")
+    with open("temporary", "wb") as receiving:
+        filedata = sock.recv(BUFFER)
+        while filedata: #will this work or will it receive the next command?
+            receiving.write(filedata)
+        
+        print("File received")
+
+
+
+
 
 
 def main():
+    
     print("Starting Server")
-
-
-    #Make directory currently in same path as the server.
     dir = sys.argv[0]
     path = make_directory(dir)
+    print("Directory setup")
     os.chdir(path) #move into this directory 
+    print("Current working directory: " + os.chdir())
 
-
-    #listening server
-    #parse data look for command. if update or add then need to loop through receiving file and creating new file then call their functs
-    #if remove then just send filename to remove function
-    #else break and close
-
-
-    #First step: constantly listening server 
-    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    #Server setup, note that only accepts one connection atm
+    print("Setting Server to Listen")
+    HOST = "0.0.0.0"  # listening on all (assuming python same as C)
     PORT = 8080      # Port to listen on (non-privileged ports are > 1023)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     sock.bind((HOST, PORT))
     sock.listen()
+    print("Server Listening for connections...") #add if statement on this
+
     conn, addr = sock.accept()
-    print('Connected to Client', addr)
+    print("Connected to Client: ", addr)
 
+    print("Listening for client commands...")
     while True:
-        data = conn.recv(4096)
+        
+        command = conn.recv(BUFFER).decode()
+        filename = conn.recv(BUFFER).decode()
+        filesize = conn.recv(BUFFER).decode()
+        filesize = int(filesize)
         print("Received data")
+        
+        if command = "add":
+            #tell client received add
+            #assume next data is file
+            file = receive_file(conn, filesize)
+            add(filename)
 
-        #checks on the data in here
+        elif command = "update":
+            #tell client received update
+            #assume next data is file
+            file = receive_file(conn, filesize)
+            update(filename)
+
+
+        elif command = "remove":
+            #tell client received remove
+            remove(filename)
+
+        else:
+            print("Command not recognised")
+            #send client message telling them
 
         if not data:
             break #break if what is received is not correct format as expecting
 
+
+
     conn.close()
-    print('client disconnected')
+    print("Client disconnected")
+    print("Shutting down server")
+    sock.close()
 
 
 if __name__ == "__main__":
