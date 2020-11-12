b7d76257f9e389a9be137060768a21e6606a8bba
cxf717 <cxf717@cs.bham.ac.uk>
2020-11-12 16:08:08 +0000

    Add sending file and monitoring directory untested code

--- pexip_client.py
+++ pexip_client.py
@@ -2,78 +2,127 @@
 ##Author: Claire Fletcher
 
 #Currently running over TCP 
+#https://www.thepythoncode.com/article/send-receive-files-using-sockets-python info on sending/receiving files as need to send in sections
+#http://timgolden.me.uk/python/win32_how_do_i/watch_directory_for_changes.html 
+
 
 import sys #needed for arguments 
 import socket
 import os
 import time 
 
+BUFFER = 4096
 
-def find_path(directory): #currently assumes the file is in the same directory as the script 
-    #may not work as using directory not file
-    path = os.path.abspath(filename)
+
+
+def get_dir_path(directory): #currently assumes the file is in the same directory as the script 
+    path = os.path.abspath(filename)#may not work as using directory not file
     return path
 
 
-#https://www.thepythoncode.com/article/send-receive-files-using-sockets-python info on sending/receiving files as need to send in sections
+def get_file_path(dir, filename):
+    #takes in the directory path and the filename and creates absolute path from this
+
+
 
 
 def dir_info(path):
-    old = {}
+    dir = {}
     for file in os.listdir(path):
         status = os.stat(file)
         mod = status.st_mtime
-        old[file] = mod
+        dir[file] = mod
     
-    return old
+    return dir
 
 
-def update():
 
 
-def add():
+def send_file(sock, filename, filepath, command):
+    filesize = os.path.getsize(filepath)
+
+    print("Connected to server")
+    sock.send(command.encode())
+    sock.send(filesize.encode())#need to encode all three of these correctly
+    sock.send(filename.encode())
+
+    with open(filename,"rb") as sending:
+        read = sending.read(BUFFER)
+        while read:
+            sock.send(read)#is send the best command? what protocol etc?
+            print("Sending file")
+            read = sending.read(BUFFER)
 
 
-def remove():
+
+
+def update(sock, filename, filepath):
+    print("Performing update command")
+    send_file(sock, filename, "update")
+
+
+
+
+def add(sock, filename, filepath):
+    print("Performing add command")
+    send_file(sock, filename, "add")
+
+
+
+
+def remove(sock, filename):
+    print("Performing remove command")
+    command = "remove"
+    sock.send(command.encode())
+    sock.send(filename.encode())
+
+
+
 
 
 def main():
     print("Starting Client")
 
     directory = sys.argv[0] #directory from arguments on command line
-    path = find_path(directory)
+    path = get_dir_path(directory)
+    print("Found directory: " + path)
 
-    #getting the old info, could do in a function
+    #First steps: connection to server
+    print("Setting up server connection...")
+    SERVER = '127.0.0.1'
+    PORT = 8080
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER,PORT))
+    #if statement then the print
+    print("Connected to Server")
+
+    #getting the old info
+    print("Monitoring Directory...")
     old = dir_info(path)
     while true:
         time.sleep(5)
         new = dir_info(path)
-        #now check if info has changed and if so call correct 
 
+        #check for add or update
+        for file in new:
+            if not file in old:
+                add(sock, file, get_file_path(dir, file))
+            
+            if #test value comparison
 
+            #check the update values
 
-
-
-    #dict store all filenames before and also all their mod times
-    #then wait
-    #then compare all filenames check for new and check for old at same time if find it then check updated 
-    #when find one then call event to send to server command and file name 
-    #file = open() then data = read() then send this along with filename 
-
-
-
-    #First steps: connection to server
-    SERVER = '127.0.0.1'
-    PORT = 8080
-
-    #initial message
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    byte_message = bytes("Hello, World!", "utf-8")
-
-    sock.connect((SERVER,PORT))
-    sock.send(byte_message)
+        #check for removals
+        for file in old:
+            if not file in new:
+                remove(sock, file)
+        
+        #set the old to the new 
+        old = new
+    
     sock.close()
+    print("Connection closed")
+
 
 
 if __name__ == "__main__":
