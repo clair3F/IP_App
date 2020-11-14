13a8609c0a0724ff74ac4fd6fc3f9185fa7f0ae8
cxf717 <cxf717@cs.bham.ac.uk>
2020-11-14 15:30:36 +0000

    Alter small code errors and tested code now working version
    Errors such as not using == and trying to use chdir instead of getcwd. Also some type issues and then some functionality such as stuck receiving a file. Also added all the commands file name and size into one packet to send. Still some missing functionality which I will add.

--- Server/pexip_server.py
+++ Server/pexip_server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ##Client side code for Synchronised folder over IP solution
 ##Author: Claire Fletcher
 
@@ -10,9 +11,10 @@ import os
 BUFFER = 4096
 
 def make_directory(dir):
-    cwdir = os.chdir() 
+    cwdir = os.getcwd() 
     path = os.path.join(cwdir, dir)
     os.mkdir(path) #note this takes a path and also throws OSError if directory exists. Handle this.
+    #if exists then don't do the mkdir and just move into it? or do error message as may not realise it exists.
     return path
 
 
@@ -33,22 +35,26 @@ def remove(filename):
 
 def add(filename):
     print("Performing add command")
-    cwdir = os.chdir() 
+    cwdir = os.getcwd() 
     oldpath = os.path.join(cwdir, "temporary")
     newpath = os.path.join(cwdir, filename)
     os.rename(oldpath, newpath)
 
 
 
-def receive_file(conn, filesize):
+def receive_file(conn, filesize, name):
     print("Receiving file")
-    with open("temporary", "wb") as receiving:
-        filedata = sock.recv(BUFFER)
-        while filedata: #will this work or will it receive the next command?
-            receiving.write(filedata)
-        
-        print("File received")
 
+    with open(name, "wb") as receiving:
+        size_left = filesize
+        while size_left: 
+            filedata = conn.recv(BUFFER)
+            receiving.write(filedata)
+            size_left -= len(filedata)
+
+        print("File received")
+#use the size for the filedata so while less than filesize and then increment it.
+#doesnt check for missing parts of files, doesn't check for file that isnt a full 4096 buffer size?
 
 
 
@@ -57,15 +63,15 @@ def receive_file(conn, filesize):
 def main():
     
     print("Starting Server")
-    dir = sys.argv[0] #could also just assume it takes in a path
+    dir = sys.argv[1] #could also just assume it takes in a path
     path = make_directory(dir)
     print("Directory setup")
     os.chdir(path) #move into this directory 
-    print("Current working directory: " + os.chdir())
+    print("Current working directory: " + os.getcwd())
 
     #Server setup, note that only accepts one connection atm
     print("Setting Server to Listen")
-    HOST = "0.0.0.0"  # listening on all (assuming python same as C)
+    HOST = "127.0.0.1"  # listening on all (assuming python same as C)
     PORT = 8080      # Port to listen on (non-privileged ports are > 1023)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,26 +86,31 @@ def main():
     print("Listening for client commands...")
     while True:
         
-        command = conn.recv(BUFFER).decode()
-        filename = conn.recv(BUFFER).decode()
-        filesize = conn.recv(BUFFER).decode()
+        receive = conn.recv(BUFFER).decode()
+        command, filesize, filename = receive.split('*')
+        print("Received command: " + command)
+        print("Received file size: " + filesize)
+        print("Received filename: " + filename)
         filesize = int(filesize)
-        print("Received data")
+
+        if command == "initial":
+            receive_file(conn, filesize, filename)
+            
         
-        if command = "add":
+        if command == "add":
             #tell client received add
             #assume next data is file
-            receive_file(conn, filesize) #should create file called temporary
+            receive_file(conn, filesize, "temporary") #should create file called temporary
             add(filename)
 
-        elif command = "update":
+        elif command == "update":
             #tell client received update
             #assume next data is file
-            receive_file(conn, filesize)
+            receive_file(conn, filesize, "temporary")
             update(filename)
 
 
-        elif command = "remove":
+        elif command == "remove":
             #tell client received remove
             remove(filename)
 
@@ -107,15 +118,13 @@ def main():
             print("Command not recognised")
             #send client message telling them
 
-        if not data:
-            break #break if what is received is not correct format as expecting
-
 
 
     conn.close()
     print("Client disconnected")
     print("Shutting down server")
     sock.close()
+    os.rmdir(path)
 
 
 if __name__ == "__main__":
